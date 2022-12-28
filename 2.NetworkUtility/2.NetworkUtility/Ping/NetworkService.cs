97a4c7240440d8146d695677debd1af04356309b
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-12-28 13:26:57 +0100

    test con strings y ints

---
+++ 2.NetworkUtility/2.NetworkUtility/Ping/NetworkService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.NetworkUtility.Ping
+{
+    public class NetworkService
+    {
+        public string SendPing()
+        {
+            return "Success: Ping Sent";
+        }
+
+        public int PingTimeout(int a, int b)
+        {
+            return a + b;
+        }
+        
+    }
+}
---
+++ 2.NetworkUtility/NetworkUtility.Test/PingTest/NetworkServiceTest.cs
@@ -0,0 +1,47 @@
+﻿using _2.NetworkUtility.Ping;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetworkUtility.Test.PingTest
+{
+    public class NetworkServiceTest
+    {
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            //Arrange 
+            var pingService = new NetworkService();
+
+            //Act
+            var result = pingService.SendPing();
+
+            //Assert
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Success: Ping Sent");
+            result.Should().Contain("Success", Exactly.Once());
+
+        }
+
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(2, 2, 4)]
+        public void NetworkService_PingTimeout_ReturnInt(int a, int b, int expected)
+        {
+            //Arrange 
+            var pingService = new NetworkService();
+
+            //Act
+            var result = pingService.PingTimeout(a, b);
+
+            //Assert
+            result.Should().Be(expected);
+            result.Should().BeGreaterThanOrEqualTo(2);
+            result.Should().NotBeInRange(-100000,0);
+        }
+    }
+}
