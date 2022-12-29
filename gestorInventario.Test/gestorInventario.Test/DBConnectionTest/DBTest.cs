a6d88809564b4eda30351777cc87c82e7ad11dec
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-12-29 13:54:12 +0100

    empezado el testing con el gestor de inventario. empezando por DBconnection los dos primeros metodos

---
+++ gestorInventario.Test/gestorInventario.Test/DBConnectionTest/DBTest.cs
@@ -0,0 +1,50 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using gestor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestorInventario.Test.DBConnectionTest
+{
+    public class DBTest
+    {
+        private readonly DBConnection _conn;
+        private readonly String _dbName;
+
+        public DBTest()
+        {
+            _conn = A.Fake<DBConnection>();
+            _dbName = "database.db";
+        }
+        
+        [Fact]
+        public void DBConnection_CreateTable_ReturnsVoid()
+        {
+            //Arrange
+            
+
+            //Act
+            _conn.CreateTable();
+
+            //Assert
+            Assert.True(File.Exists(_dbName));
+        }
+
+        [Fact]
+        public void DBConnection_DropTable_ReturnsBool()
+        {
+            //Arrange
+
+            //Act
+            var x= _conn.DropTable();
+
+            //Assert
+            x.Should().BeTrue();
+            x.Should().NotBe(false);
+            x.Should().Be(true);
+        }
+    }
+}
---
+++ gestorInventario.Test/gestorInventario.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace gestorInventario.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
