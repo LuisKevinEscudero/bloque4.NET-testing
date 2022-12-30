2616f1ca792009bef89f34356187b9d47696fe7e
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-12-30 13:24:12 +0100

    intento de test menu

---
+++ gestorInventario.Test/gestorInventario.Test/MenuTest/TestMenu.cs
@@ -0,0 +1,28 @@
+﻿using FakeItEasy;
+using gestor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestorInventario.Test.MenuTest
+{
+    public class TestMenu
+    {
+        private readonly DBConnection _conn;
+        private readonly String _dbName;
+        private readonly Console consol;
+        public TestMenu()
+        {
+            _conn = A.Fake<DBConnection>();
+            _dbName = "database.db";
+        }
+
+        [Fact]
+        public void Menu_Start_ReturnsVoid()
+        {
+            Console console = new A().Fake<Console>();
+        }
+    }
+}
